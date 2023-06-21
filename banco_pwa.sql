88ce058aa4f1abb1c5c2ef4d4aa6acab88d8e6da
Luan Simões <luanc179@gmail.com>
2023-06-20 23:50:25 -0300

    banco

--- banco_pwa.sql
+++ banco_pwa.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: localhost    Nomebase: pwa
+-- Host: localhost    Database: pwa
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -25,8 +25,8 @@ DROP TABLE IF EXISTS `livro`;
 CREATE TABLE `livro` (
   `id` varchar(45) NOT NULL,
   `titulo` varchar(45) DEFAULT NULL,
-  `autor` varchar(45) DEFAULT NULL,
   `genero` varchar(45) DEFAULT NULL,
+  `valor` decimal(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,8 +37,36 @@ CREATE TABLE `livro` (
 
 LOCK TABLES `livro` WRITE;
 /*!40000 ALTER TABLE `livro` DISABLE KEYS */;
-INSERT INTO `livro` VALUES ('ewtertq','Branca de neve','sei la','viadagem'),('ewwegF','Os Luzíadas','Camões','Aventura');
+INSERT INTO `livro` VALUES ('ae8e87a0-04bd-4bba-acbd-6e6e484d4b13','Luan','sei la',100.00),('erte234rt','HP','Fantasia',60.00),('erterewtet','SA','Aventura',80.00),('iwuuryuiwy','João e Maria','Fantasia',25.00);
 /*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livro_autores`
+--
+
+DROP TABLE IF EXISTS `livro_autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `livro_autores` (
+  `id` varchar(45) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `sobrenome` varchar(45) DEFAULT NULL,
+  `idlivro` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idlivro_idx` (`idlivro`),
+  CONSTRAINT `idlivro` FOREIGN KEY (`idlivro`) REFERENCES `livro` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livro_autores`
+--
+
+LOCK TABLES `livro_autores` WRITE;
+/*!40000 ALTER TABLE `livro_autores` DISABLE KEYS */;
+INSERT INTO `livro_autores` VALUES ('3d072510-afc3-47c7-a00a-58449302d26f','Luan','Simões','ae8e87a0-04bd-4bba-acbd-6e6e484d4b13'),('7b005504-9d12-4bae-a674-b08fb9cc75f9','Luan','Fones','ae8e87a0-04bd-4bba-acbd-6e6e484d4b13');
+/*!40000 ALTER TABLE `livro_autores` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-20  0:13:45
+-- Dump completed on 2023-06-20 23:49:10
